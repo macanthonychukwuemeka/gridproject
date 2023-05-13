da5d7a6e4d2fc2041a3a6cae312c3685e0a19267
macanthonychukwuemeka <57002500+macanthonychukwuemeka@users.noreply.github.com>
2023-05-13 13:14:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// declaration of varible
+// let, var, const 
+
+// let name = 'abuchi'; var age = 32; const school = 3;
+//  var title = document.getElementById('til')
+//  title.innerText = name
+
+
+  let array = [names="abuchi", age="32",schoosl="esut"]
+  console.log(array[0])
+
+
+
+   let obj = {
+     student: [
+        name = "abuchi",
+         schools= "esut",
+         age = "32"
+
+     ],
+      gardian: [
+        mothername = "mum",
+        bro = "obi"
+      ]
+   }
+
+
+   console.table(obj.student)
+
+   
+ 
+   console.log("monday")
+
+/*
+=========================
+hero Element 
+=========================
+*/
+
+let message = "goodbye"
+  
+var header = document.getElementById('til')
+console.log(header)
+header.innerText= message
+ 
+  var tech_gadgets= [
+    {
+ type: "communincation_and_media_gadgets",
+ list:[
+    "Smartphone",
+    "Tablet",
+    "Headphone and earbud",
+    "Wireless Chargers",
+]
+},
+ {
+   type: "Computer Gadgets",
+    list:[
+        "laptop",
+        "gaming mouse",
+        "monitor for gaming",
+        "game controller",
+        "windows mini pc",      
+        ]
+    },
+    {
+        type: "Apple Gadget",
+        list: [
+            "iphone",
+            "Macbook M1",
+            "Appliances and Smart Home Gadgets",
+        ]
+    },
+  ]
